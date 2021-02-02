4c85121322c7f62db85fcf8d6812bc8ad956f3d4
nothub <48992448+nothub@users.noreply.github.com>
2021-02-02 15:22:12 +0100

    api stress test

---
+++ tests/stress.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+THREADS=100
+LOOPS=8
+
+START=$(date +%s.%N)
+
+i=0;
+for ((i = 0; i < THREADS; i++)); do
+    for ((j = 0; j < LOOPS; j++)); do
+        curl http://127.0.0.1:5000/servers/0b0t.org &>/dev/null &
+    done
+    pids[${i}]=$!
+done
+
+echo "started $THREADS threads with $LOOPS loops"
+
+for pid in ${pids[*]}; do
+    wait $pid
+done
+
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+
+echo "done in $DIFF sec"
